355318d7dcd2376ca713454e46c353c41c347297
abysmli <abysmli@gmail.com>
2015-07-04 22:06:22 +0200

    localized affilinet api utils

--- routes/affilinetapi.js
+++ routes/affilinetapi.js
@@ -1,83 +1,87 @@
 var express = require('express');
 var router = express.Router();
-var Affilinet = require('affilinet');
-	
+var Affilinet = require('../utils/affilinetapis.js');
+
 var affilinet = new Affilinet({
-	publisherId: '512499',
-	productWebservicePassword: 'FaI69alVX0eZ4i28TnIq',
-	publisherWebservicePassword: 'lZONYNI32ieYq8kMPqKS'
+    publisherId: '512499',
+    productWebservicePassword: 'FaI69alVX0eZ4i28TnIq',
+    publisherWebservicePassword: 'lZONYNI32ieYq8kMPqKS'
 });
 
-router.get('/getShops', function(req, res, next) {
-	affilinet.getShops(function(err, shops) {
-		if (err != null) {
-			throw err;
-		}
+router.get('/getShops', function (req, res, next) {
+    affilinet.getShops(function (err, shops) {
+        if (err != null) {
+            throw err;
+        }
 
-		res.json(shops);
-	});
+        res.json(shops);
+    });
 });
 
-router.get('/getCategorys', function(req, res, next) {
-	affilinet.getCategorys(function(err, categorys) {
-		if (err != null) {
-			throw err;
-		}
+router.get('/getCategorys', function (req, res, next) {
+    affilinet.getCategorys(function (err, categorys) {
+        if (err != null) {
+            throw err;
+        }
 
-		res.json(categorys);
-	});
+        res.json(categorys);
+    });
 });
 
-router.get('/getProducts', function(req, res, next) {
-	affilinet.getProducts(function(err, products) {
-		if (err != null) {
-			throw err;
-		}
+router.get('/getProducts', function (req, res, next) {
+    affilinet.getProducts(function (err, products) {
+        if (err != null) {
+            throw err;
+        }
 
-		res.json(products);
-	});
+        res.json(products);
+    });
 });
 
-router.get('/getSalesForDayRange', function(req, res, next) {
-	affilinet.getSalesLeadsBySubIdPerDay(new Date("2014-06-01"), new Date(), function(err, sales) {
-		if (err != null) {
-			throw err;
-		}
-		res.json(sales);
-	});
+router.get('/getSalesForDayRange', function (req, res, next) {
+    affilinet.getSalesLeadsBySubIdPerDay(new Date("2014-06-01"), new Date(), function (err, sales) {
+        if (err != null) {
+            throw err;
+        }
+        res.json(sales);
+    });
 });
 
-router.get('/getPrograms', function(req, res, next) {
-	var displayOptions = {
-		CurrentPage: 1,
-		PageSize: 100,
-	};
-	var query = {
-		// "Active", "Paused", "Waiting", "Refused", "NoPartnership", "Cancelled"
-		PartnershipStatus: ["Active"],
-	};
-	affilinet.getPrograms(displayOptions, query, function(err, programs) {
-		if (err != null) {
-			throw err;
-		}
-		res.json(programs);
-	});
+router.get('/getPrograms', function (req, res, next) {
+    var displayOptions = {
+        CurrentPage: 1,
+        PageSize: 100,
+    };
+    var query = {
+        // "Active", "Paused", "Waiting", "Refused", "NoPartnership", "Cancelled"
+        PartnershipStatus: ["Active"],
+    };
+    affilinet.getPrograms(displayOptions, query, function (err, programs) {
+        if (err != null) {
+            throw err;
+        }
+        res.json(programs);
+    });
 });
 
-router.get('/searchCreatives', function(req, res, next) {
-	var displayOptions = {
-		CurrentPage: 1,
-		PageSize: 5,
-	};
-	var query = {
-		ProgramIds: [0]
-	};
-	affilinet.searchCreatives(displayOptions, query, function(err, creatives) {
-		if (err != null) {
-			throw err;
-		}
-		res.json(creatives);
-	});
+router.get('/searchCreatives', function (req, res, next) {
+    var displayOptions = {
+        CurrentPage: 1,
+        PageSize: 5,
+    };
+    var query = {
+        ProgramIds: [0]
+    };
+    affilinet.searchCreatives(displayOptions, query, function (err, creatives) {
+        if (err != null) {
+            throw err;
+        }
+        res.json(creatives);
+    });
+});
+
+router.get('/test', function (req, res, next) {
+
 });
 
 module.exports = router;
