2f75b900ddf6e2b428263412662579440b018af3
abysmli <abysmli@gmail.com>
2015-07-07 13:03:11 +0200

    syn

--- utils/php/getProductbyCategory.php
+++ utils/php/getProductbyCategory.php
@@ -6,6 +6,8 @@ include_once("ProductService.Affilinet.php");
 $Username = $argv[1]; // the publisher ID
 $Password = $argv[2]; // the product data web services password
 $CategoryID = $argv[3];
+$CurrentPage = $argv[4];
+$Pagesize = $argv[5];
 
 $ps= new ProductService();
 $ps->Affilinet($Username, $Password, 'Product');
--- utils/php/getProductbyShop.php
+++ utils/php/getProductbyShop.php
@@ -6,6 +6,8 @@ include_once("ProductService.Affilinet.php");
 $Username = $argv[1]; // the publisher ID
 $Password = $argv[2]; // the product data web services password
 $ShopID = $argv[3];
+$CurrentPage = $argv[4];
+$Pagesize = $argv[5];
 
 $ps= new ProductService();
 $ps->Affilinet($Username, $Password, 'Product');
