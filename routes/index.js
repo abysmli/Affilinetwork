92c2e3153459c23c9f82f719460d1bee6c5c4090
abysmli <abysmli@gmail.com>
2015-07-08 14:06:41 +0200

    beta1

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,24 @@ var Affilinet = require('../utils/affilinetapis.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    Product.find({}, null, {
-            limit: 100,
+    Product.count({}, function (err, count) {
+        Product.find({}, null, {
+            limit: 30,
             sort: {
                 updated_at: -1
             }
         }, function (err, products) {
-        if (err != null) res.render('error');
-        else {
-            res.render('home', {
-                title: 'Home Page',
-                products: products,
-                layout: '/user_layout'
-            });
-        }
+            if (err != null) res.render('error');
+            else {
+                res.render('home', {
+                    title: 'Home Page',
+                    count: count,
+                    products: products,
+                    layout: '/user_layout'
+                });
+            }
+        });
     });
-
 });
 
 module.exports = router;
