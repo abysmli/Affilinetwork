93dc816962cd3fc5d37ee117c9fd6f99ae3f0841
Li, Yuan <abysmli@gmail.com>
2018-03-06 18:12:51 +0100

    update images url

--- updateImageURL.js
+++ updateImageURL.js
@@ -1,22 +1,25 @@
+const mongoose = require('mongoose');
 const Product = require('./models/product');
-
-let product_sum = 0;
-Product.find().exec((err, products) => {
-    console.log(`Product Sum: ${products.length}`);
-    products.forEach(product => {
-        product.ProductImage = product.ProductImage.split('?')[0];
-        product.ProductImageSet.forEach(imageurl => {
-            imageurl = imageurl.split('?')[0];
-        });
-        product.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(`${product_sum++} : ${product.EAN}`);
-                if (product_sum === products.length) {
-                    process.exit(0);
+mongoose.connect('mongodb://localhost/aok-china');
+(() => {
+    let product_sum = 0;
+    Product.find().exec((err, products) => {
+        console.log(`Product Sum: ${products.length}`);
+        products.forEach(product => {
+            product.ProductImage = product.ProductImage.split('?')[0];
+            product.ProductImageSet.forEach(imageurl => {
+                imageurl = imageurl.split('?')[0];
+            });
+            product.save((err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`${product_sum++} : ${product.EAN}`);
+                    if (product_sum === products.length) {
+                        process.exit(0);
+                    }
                 }
-            }
+            });
         });
     });
-});
+})();
