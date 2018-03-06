4f5290ef8281a8e388941aacd34f8bf64d758130
Li, Yuan <abysmli@gmail.com>
2018-03-06 18:20:04 +0100

    update images url

--- updateImageURL.js
+++ updateImageURL.js
@@ -1,29 +1,36 @@
 const mongoose = require('mongoose');
 const Product = require('./models/product');
-var setting = require('./setting');
+let setting = require('./setting');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost/' + setting.database, {
     useMongoClient: true
 });
+var _products = [];
 (() => {
     let product_sum = 0;
     Product.find().exec((err, products) => {
         console.log(`Product Sum: ${products.length}`);
-        products.forEach(product => {
-            product.ProductImage = product.ProductImage.split('?')[0];
-            product.ProductImageSet.forEach(imageurl => {
-                imageurl = imageurl.split('?')[0];
-            });
-            product.save((err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(`${product_sum++} : ${product.EAN}`);
-                    if (product_sum === products.length) {
-                        process.exit(0);
-                    }
-                }
-            });
-        });
+        _products = products;
+        updateURL(0);
     });
 })();
+
+function updateURL(i) {
+    let product = _products[i];
+    product.ProductImage = product.ProductImage.split('?')[0];
+    product.ProductImageSet.forEach(imageurl => {
+        imageurl = imageurl.split('?')[0];
+    });
+    product.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`${i+1} : ${product.EAN}`);
+            if ((i + 1) === _products.length) {
+                process.exit(0);
+            } else {
+                updateURL(i + 1);
+            }
+        }
+    });
+}
