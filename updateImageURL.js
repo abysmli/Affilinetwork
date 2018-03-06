e6858c28260305ef6b3e0e53ecb3defafa8e8c53
Li, Yuan <abysmli@gmail.com>
2018-03-06 20:53:12 +0100

    fix bug

--- updateImageURL.js
+++ updateImageURL.js
@@ -17,23 +17,28 @@ mongoose.connect('mongodb://localhost/' + setting.database, {
         _products = products;
         updateURL(0);
     });
+
     function updateURL(i) {
         let product = _products[i];
-        product.ProductImage = product.ProductImage.split('?')[0];
-        product.ProductImageSet.forEach(imageurl => {
-            imageurl = imageurl.split('?')[0];
-        });
-        product.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(`${i+1} : ${product.EAN}`);
-                if ((i + 1) === _products.length) {
-                    process.exit(0);
-                } else {
-                    updateURL(i + 1);
-                }
+        if (product.ProductImage) {
+            product.ProductImage = product.ProductImage.split('?')[0];
+            if (product.ProductImageSet.length !== 0) {
+                product.ProductImageSet.forEach(imageurl => {
+                    imageurl = imageurl.split('?')[0];
+                });
             }
-        });
+            product.save((err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`${i+1} : ${product.EAN}`);
+                    if ((i + 1) === _products.length) {
+                        process.exit(0);
+                    } else {
+                        updateURL(i + 1);
+                    }
+                }
+            });
+        }
     }
 })();
