dec0a38b2540b3e16b8fd7d5731ff181b2e9721c
Li, Yuan <abysmli@gmail.com>
2018-03-06 18:09:42 +0100

    update images url

---
+++ updateImageURL.js
@@ -0,0 +1,22 @@
+const Product = require('./models/product');
+
+let product_sum = 0;
+Product.find().exec((err, products) => {
+    console.log(`Product Sum: ${products.length}`);
+    products.forEach(product => {
+        product.ProductImage = product.ProductImage.split('?')[0];
+        product.ProductImageSet.forEach(imageurl => {
+            imageurl = imageurl.split('?')[0];
+        });
+        product.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`${product_sum++} : ${product.EAN}`);
+                if (product_sum === products.length) {
+                    process.exit(0);
+                }
+            }
+        });
+    });
+});
